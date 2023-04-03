ac95d33345aa45168b0c2f149cda93ca7e0b769b
emobilis <samuelkahigakimani@gmail.com>
2023-04-03 10:21:49 +0300

    Android Authentication

---
+++ app/src/main/java/com/example/android_authentication/Register_activity.kt
@@ -0,0 +1,11 @@
+package com.example.android_authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Register_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
